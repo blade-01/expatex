ab614173a18723b1a6b877b952962e0d8b58365a
blade-01 <animashauntaofiq@gmail.com>
2022-01-29 17:51:02 +0100

    Update: Working on color guides screen

--- src/views/ColorGuide.vue
+++ src/views/ColorGuide.vue
@@ -1,13 +1,26 @@
 <template>
-  <div></div>
+  <the-header />
+  <div class="color-guide">
+    <div class="container">
+      <div class="color-guide-content routes"></div>
+    </div>
+  </div>
+  <home-testimonies />
+  <the-footer />
 </template>
 
 <script>
+import TheHeader from "@/components/navbar/TheHeader.vue";
+import TheFooter from "@/components/navbar/TheFooter.vue";
+
 export default {
-  name: "GalleryView",
+  name: "HomeView",
+  components: {
+    TheHeader,
+    TheFooter,
+  },
 };
 </script>
-
 <style lang="scss" scoped>
 @import "@/scss/style.scss";
 </style>
